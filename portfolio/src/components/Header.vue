a35a59f633c2e1c75707048f4478a31ac38bb240
Ignotik <zhdanovignatio@yandex.ru>
2024-08-25 18:42:33 +0300

    Final

--- portfolio/src/components/Header.vue
+++ portfolio/src/components/Header.vue
@@ -1,6 +1,8 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <header class="max-w-xs mx-auto mt-4 pb-4 px-4 border-b-4 border-slate-500">
+  <header
+    class="max-w-screen-xl mx-auto mt-4 pb-4 px-4 border-b-4 border-slate-500"
+  >
     <ul class="flex font-mono gap-8 justify-between">
       <li class="hover:text-lime-400">
         <RouterLink to="/">Главная</RouterLink>
